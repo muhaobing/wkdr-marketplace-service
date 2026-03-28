05b3b5a279695e225d91de19c9d99e2b596d4b1f
haobing.mu <muhaobing@gmail.com>
2026-03-28 22:11:27 +0800

    fix code

--- internal/common/constant/err_code/err_code.go
+++ internal/common/constant/err_code/err_code.go
@@ -4,9 +4,9 @@ package err_code
 const (
 	// Success 成功
 	Success = 0
-	// General 未单独分类的错误（默认）
-	General = -1
+	// CommonError 未单独分类的错误（默认）
+	CommonError = -1
 
 	// UserBindingNotFound 业务身份 biz_code + biz_user_id 在商城未绑定商城用户
-	UserBindingNotFound = 100404
+	UserBindingNotFound = -100404
 )
